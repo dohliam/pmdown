6ea9fddb12c506eee9d5e82bd65cffbe6d7548f4
dohliam <dohliam@gmail.com>
2021-08-08 09:43:53 -0700

    move ruby methods to library

---
+++ rb/convert_pm2md.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+# convert Markdown (.md) files to PmWiki (.pmwiki) format
+
+require_relative 'libpm2md.rb'
+
+filename = ARGF
+if !filename
+  abort("Please specify a file to convert.")
+elsif !File.exist?(filename)
+  abort("Specified file does not exist.")
+end
+
+# md_file = File.read(filename)
+md_file = filename.read
+
+# puts md_to_pm(md_file)
+puts pm_to_md(md_file)
--- rb/libpm2md.rb
+++ rb/libpm2md.rb
@@ -1,7 +1,3 @@
-#!/usr/bin/env ruby
-
-# convert Markdown (.md) files to PmWiki (.pmwiki) format
-
 def md_to_pm(text)
   text
     .gsub(/(^|\s)\*\*(.*?)\*\*/, "'''\\2'''")
@@ -25,16 +21,3 @@ def pm_to_md(text)
     .gsub(/'''(.*?)'''/, "**\\1**")
     .gsub(/''(.*?)''/, "_\\1_")
 end
-
-filename = ARGF
-if !filename
-  abort("Please specify a file to convert.")
-elsif !File.exist?(filename)
-  abort("Specified file does not exist.")
-end
-
-# md_file = File.read(filename)
-md_file = filename.read
-
-# puts md_to_pm(md_file)
-puts pm_to_md(md_file)
